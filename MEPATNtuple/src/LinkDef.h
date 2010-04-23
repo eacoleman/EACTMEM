d038aa2c50b572f4baa9687c4f46de3573ee9f80
ilyao <>
2010-04-23 23:08:37 +0000

    Defines the basic cuts & tools necessary to skim the pats and produce tables.

--- MEPATNtuple/src/LinkDef.h
+++ MEPATNtuple/src/LinkDef.h
@@ -1,5 +1,6 @@
 #include "TAMUWW/MEPATNtuple/interface/METree.hh"
 #include "TAMUWW/MEPATNtuple/interface/MicroNtuple.hh"
+#include "TAMUWW/MEPATNtuple/interface/EventNtuple.hh"
 
 #ifdef __CINT__
 #pragma link off all globals;
@@ -9,6 +10,9 @@
 #pragma link C++ class METree;
 #pragma link C++ class ProbStat;
 #pragma link C++ class MicroNtuple;
+#pragma link C++ class EventNtuple;
 #pragma link C++ class DumbClassToTrickRoot;
+//#pragma link C++ class DumbClassToTrickRoot2;
+
 
 #endif
