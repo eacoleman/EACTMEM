f230d8d1e5d0ad167ca353487440066ed38c5579
aperloff <>
2011-04-27 17:17:14 +0000

    Added functionality to the parseFromFile member function

--- SpecialTools/interface/FileLocationTable.hh
+++ SpecialTools/interface/FileLocationTable.hh
@@ -1,7 +1,7 @@
 // Ricardo Eusebi
 // FNAL eusebi@fnal.gov
 // created: Monday February 05, 2007
-// $Id: FileLocationTable.hh,v 1.1 2011/02/08 21:31:38 eusebi Exp $
+// $Id: FileLocationTable.hh,v 1.1 2011/03/01 02:02:45 eusebi Exp $
 
 #ifndef FILELOCATIONTABLE_DEF
 #define FILELOCATIONTABLE_DEF
@@ -23,7 +23,16 @@ public :
   
   FileLocationTable(std::string name = "FileLocationTable");
  
-  void addBasePath(std::string, std::string rowNameToOmit="BasePath");
+  void addBasePath();
+
+  std::string getBasePath() {return basePath;}
+
+  virtual bool parseFromFile(std::string filename, std::string cellClass = "TableCellText", std::string formatStyle = "Normal");
+
+private :
+
+  std::string basePath;
+  bool basePathAdded;
 
   ClassDef (FileLocationTable,1)
 };
