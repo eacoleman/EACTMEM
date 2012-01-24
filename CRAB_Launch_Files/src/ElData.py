0ef11fb3826b0fc1f940e2618c06ac9522c5a0ae
ilyao <>
2012-01-24 20:10:01 +0000

    Additional cleanup.

--- CRAB_Launch_Files/src/ElData.py
+++
@@ -1,7 +0,0 @@
-#################################################################################################
-#### CMS 
-#### Preliminary Sensitivity Analysis
-#### Created by Osipenkov, Ilya : ilyao@fnal.gov
-#################################################################################################
-from CRABLaunch_Any_V4Data import *
-process.out.fileName = 'ElData.root'
