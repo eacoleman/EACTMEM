22c9fe8989594773e43d953e609dd8ce8700d9d9
tlamb <>
2012-07-13 20:41:04 +0000

    Major update to rebin tester that puts it in a working state

--- Tools/bin/TestRebinBGEst_x.cc
+++ Tools/bin/TestRebinBGEst_x.cc
@@ -10,39 +10,64 @@
 // TAMU Libraries
 #include "TAMUWW/Tools/interface/BackgroundEstimator.hh"
 
+// Root Libraries
+#include "TH1.h"
+#include "TFile.h"
+
 using namespace std;
 
 int main()
 {
    BackgroundEstimator testBGEst("electron", "MET", "electron.root", "RebinTest_el_MET.root");
    
-   //vector<double> parameterZero;
-   //vector<double> parameterOne;
+   vector<double> parameterZero;
+   vector<double> parameterOne;
+   vector<double> chiSquared;
    
-   //parameterZero.push_back(0);
-   //parameterOne.push_back(0);
+   parameterZero.push_back(0);
+   parameterOne.push_back(0);
+   chiSquared.push_back(0);
    
-   //for(unsigned int i = 1; i <= 20; i++)
-   //{
-   //cout << "Running a test with rebin size = " << i << "..." << endl;
+   for(unsigned int i = 1; i <= 20; i++)
+   {
+      cout << "Running a test with rebin size = " << i << "..." << endl;
       
-   //testBGEst.setRebinSizeDEBUG(i);
+      testBGEst.setRebinSizeDEBUG(i);
       testBGEst.readHistograms();
       
-      //const double* parameters = testBGEst.fitHistograms();
+      testBGEst.fitHistograms();
       
-      //parameterZero.push_back(parameters[0]);
-      //parameterOne.push_back(parameters[1]);
+      pair<double, double> parameters = testBGEst.getParameters();
       
-      //cout << "Par0:" << parameters[0] << "\tPar1:" << parameters[1] << endl;
-      //}
+      parameterZero.push_back(parameters.first);
+      parameterOne.push_back(parameters.second);
+      chiSquared.push_back(testBGEst.getReducedChiSquared());
+      
+      cout << "Par0:" << parameters.first << "\tPar1:" << parameters.second << endl;
+   }
    
-      //cout << endl << "##### RESULTS #####" << endl;
-      //cout << "Rebin\tPar0\tPar1" << endl;
-      //for(unsigned int i = 1; i <= 20; i++)
-      //{
-      //cout << i << "\t" << parameterZero[i] << "\t" << parameterOne[i] << endl;
-      //}
+   TH1D* parZeroPlot = new TH1D("ParameterZero", "ParameterZero", 15, 0, 1.5);
+   TH1D* parOnePlot = new TH1D("ParameterOne", "ParameterOne", 30, 0, 1.5);
+   
+   cout << endl << "##### RESULTS #####" << endl;
+   cout << "Rebin   Par0   Par1   Chi2" << endl;
+   for(unsigned int i = 1; i <= 20; i++)
+   {
+      cout << i << "   " << parameterZero[i] << "   " << parameterOne[i] << "   " << chiSquared[i] << endl;
+      
+      parZeroPlot->Fill(parameterZero[i], 1/chiSquared[i]);
+      parOnePlot->Fill(parameterOne[i], 1/chiSquared[i]);
+   }
+   
+   parZeroPlot->GetXaxis()->SetTitle("QCD Scale Factor");
+   parOnePlot->GetXaxis()->SetTitle("WJets Scale Factor");
+   
+   TFile output("parameterPlots.root", "RECREATE");
+   
+   parZeroPlot->Write();
+   parOnePlot->Write();
+   
+   output.Close();
    
    cout << "Done." << endl;
    return 0;
