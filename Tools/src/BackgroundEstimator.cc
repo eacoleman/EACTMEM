afd327e18ff1417cc441c968a29b15cafa5b1dad
tlamb <>
2012-07-17 17:20:29 +0000

    Changed from QCD to QCD_MuEnriched

--- Tools/src/BackgroundEstimator.cc
+++ Tools/src/BackgroundEstimator.cc
@@ -110,7 +110,7 @@ void BackgroundEstimator::fitHistograms()
 {
    const double* parameters = fitAndReturnParameters();
    
-   Histograms::monteCarloHistograms["QCD"]->Scale(parameters[0]);
+   Histograms::monteCarloHistograms["QCD_MuEnriched"]->Scale(parameters[0]);
    Histograms::monteCarloHistograms["WJets"]->Scale(parameters[1]);
    
    mcStack->Modified();
@@ -135,7 +135,7 @@ void BackgroundEstimator::writeHistograms()
    TPad* inputPad;
    THStack* inputMCStack;
    TList* inputMCList;
-   TH1D* histQCD;
+   TH1D* histQCD_MuEnriched;
    TH1D* histWJets;
    
    for(unsigned int i = 0; i < plotPrefixes.size(); i++)
@@ -147,10 +147,10 @@ void BackgroundEstimator::writeHistograms()
       inputMCStack = (THStack*) inputPad->GetPrimitive((prefix + "_stackMC").c_str());
       inputMCList = inputMCStack->GetHists();
       
-      histQCD = (TH1D*)inputMCList->FindObject((prefix + "_QCD").c_str());
+      histQCD_MuEnriched = (TH1D*)inputMCList->FindObject((prefix + "_QCD_MuEnriched").c_str());
       histWJets = (TH1D*)inputMCList->FindObject((prefix + "_WJets").c_str());
       
-      histQCD->Scale(scaleParameters.first);
+      histQCD_MuEnriched->Scale(scaleParameters.first);
       histWJets->Scale(scaleParameters.second);
       
       inputMCStack->Modified();
@@ -216,7 +216,7 @@ void BackgroundEstimator::initializeHistNames()
    histNames.push_back("WJets");
    histNames.push_back("DYJets");
    histNames.push_back("TTbar");
-   histNames.push_back("QCD");
+   histNames.push_back("QCD_MuEnriched");
    histNames.push_back("ggH125");
 }
 
@@ -319,10 +319,10 @@ double BackgroundEstimator::fitFunc(const double *par)
       double data = 0;
       double dataError2 = 0;
       
-      mc += par[0]*Histograms::monteCarloHistograms["QCD"]->GetBinContent(bin);
+      mc += par[0]*Histograms::monteCarloHistograms["QCD_MuEnriched"]->GetBinContent(bin);
       mc += par[1]*Histograms::monteCarloHistograms["WJets"]->GetBinContent(bin);
       
-      mcError2 += pow(par[0]*Histograms::monteCarloHistograms["QCD"]->GetBinError(bin), 2);
+      mcError2 += pow(par[0]*Histograms::monteCarloHistograms["QCD_MuEnriched"]->GetBinError(bin), 2);
       mcError2 += pow(par[1]*Histograms::monteCarloHistograms["WJets"]->GetBinError(bin), 2);
       
       data += Histograms::dataHistogram->GetBinContent(bin);
@@ -330,7 +330,7 @@ double BackgroundEstimator::fitFunc(const double *par)
    
       for(map<string, TH1D*>::iterator it = Histograms::monteCarloHistograms.begin(); it != Histograms::monteCarloHistograms.end(); it++)
       {
-         if( ((*it).first != "QCD") && ((*it).first != "WJets") )
+         if( ((*it).first != "QCD_MuEnriched") && ((*it).first != "WJets") )
          {
             mc += (*it).second->GetBinContent(bin);
             mcError2 += pow((*it).second->GetBinError(bin), 2);
